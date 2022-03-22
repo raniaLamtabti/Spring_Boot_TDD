d41ad3df461da7a3edf961d7f3d5dcdf1aa9a129
Rania Lamtabti <rania.lamtabti@gmail.com>
2022-03-22 14:03:53 +0100

    initial spring boot
    initial simple spring boot project allowing the management of the customers of a B2C company.

---
+++ Client_Management_B2C/src/main/java/com/youcode/client_management_b2c/ClientManagementB2CApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.client_management_b2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagementB2CApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientManagementB2CApplication.class, args);
+    }
+
+}
---
+++ Client_Management_B2C/src/test/java/com/youcode/client_management_b2c/ClientManagementB2CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.client_management_b2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagementB2CApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
