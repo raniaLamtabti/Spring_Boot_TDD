83c8f9084fc4ead955e71d87d7bf16ad7a679352
Rania Lamtabti <rania.lamtabti@gmail.com>
2022-03-22 14:27:06 +0100

    create save method in ClientService and findByEmail in ClientRepository
    save method help us to save a single client in the DB after checking his existence using findByEmail method

--- Client_Management_B2C/src/main/java/com/youcode/client_management_b2c/services/ClientService.java
+++ Client_Management_B2C/src/main/java/com/youcode/client_management_b2c/services/ClientService.java
@@ -1,4 +1,21 @@
 package com.youcode.client_management_b2c.services;
 
+import com.youcode.client_management_b2c.entities.Client;
+import com.youcode.client_management_b2c.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class ClientService {
+    @Autowired
+    ClientRepository clientRepository;
+
+    public Client save(Client client){
+
+        Optional<Client> email =clientRepository.findByEmail(client.getEmail());
+        if (email.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        return clientRepository.save(client);
+    }
 }
