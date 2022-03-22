e2d77b38b243c259d1da8210e3b9c31c1f828f1c
Rania Lamtabti <rania.lamtabti@gmail.com>
2022-03-22 16:36:26 +0100

    generate test for ClientController

---
+++ Client_Management_B2C/src/test/java/com/youcode/client_management_b2c/controllers/ClientControllerTest.java
@@ -0,0 +1,36 @@
+package com.youcode.client_management_b2c.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientControllerTest {
+
+    @Test
+    void addClients() {
+    }
+
+    @Test
+    void getClients() {
+    }
+
+    @Test
+    void getClientById() {
+    }
+
+    @Test
+    void getClientByEmail() {
+    }
+
+    @Test
+    void getClientsByGender() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+}
